c75b72df2ac08b37dd54904ac60498c8df19c073
Christodoulos <43905544+chriskalo@users.noreply.github.com>
2020-05-18 20:29:33 +0300

    Update html-processor.py

--- html-processor.py
+++ html-processor.py
@@ -1,6 +1,6 @@
 import re
 
-def myfunction(m):  #ilopoihsh sunarthshs pou tha kaleite meso ths sub kai tha epistrefei to antistoixo string kathe fora analoga pou to teriasma pou exei ginei 
+def myfunction(m):  #ilopoihsh sunarthshs pou tha kaleite meso ths sub kai tha epistrefei to antistoixo string kathe fora analoga me to teriasma pou exei ginei 
   if (m.group(0)=='&amp;'):
     return '&'
   
@@ -24,9 +24,9 @@ rexp4 = re.compile(r'<a.+?href="(.*?)".*?>(.*?)</a>',re.DOTALL) #teriasma olwn t
 rexp5_1 = re.compile(r'<.+?>|</.+?>',re.DOTALL) #teriasma olwn twn tag pou exoun morfh p.x <a> h </a> 
 rexp5_2 = re.compile(r'<.+?/>',re.DOTALL) #teriasma olwn twn tag  pou exoun morfh p.x <meta charset="utf-8" />
 
-rexp6 = re.compile(r'&(amp|gt|lt|nbsp);') # teriasma olwn twn akolouthiwn pou exoun morfh &amp; h &gt; h &lt; h &nbsp; mesa sto keimeno
+rexp6 = re.compile(r'&(amp|gt|lt|nbsp);') #teriasma olwn twn akolouthiwn pou exoun morfh &amp; h &gt; h &lt; h &nbsp; mesa sto keimeno
 
-rexp7 = re.compile(r'\s+') #teriasma akolouthiwn sinexomenwn xarakthrwn whitespace
+rexp7 = re.compile(r'\s+') #teriasma akolouthiwn sinexomenwn xarakthrwn whitespace mesa sto keimeno
 
 with open('testpage.txt','r') as fp:
 
@@ -41,14 +41,14 @@ with open('testpage.txt','r') as fp:
   text = rexp3.sub(' ',text) #apaloifh olwn twn tag <script></script> kai <style></style> mesa sto keimeno mazi me to periexomeno tous
 
   for m in rexp4.finditer(text): #anazhthsh kathe pithanou teriasmatos mesa sto keimeno simfona me thn mhxanh teriasmatos rexp4(parapanw perigrafetai h kanonikh ekfrash vash ths opoias kanei anazhthsh tairiasmatos h rexp4)
-    print('{}    {}'.format(m.group(1),m.group(2))) #ektiposh merous tou teriasmatos(sindesmos kai periexomeno metaksi <a> kai </a>) pou vrike h mhxanh teriasmatos rexp4
+    print('{}    {}'.format(m.group(1),m.group(2))) #ektiposh merous tou kathe teriasmatos(sindesmos ths idiothtas href kai periexomeno metaksi <a> kai </a>) pou vrike h mhxanh teriasmatos rexp4
 
-  text = rexp5_1.sub(' ',text) #apaloifh olwn twn tag tou keimenou ths morfhs <a> h </a>
-  text = rexp5_2.sub(' ',text) #apaloifh olwn twn tag tou keimenou ths morfhs <meta charset="utf-8" />
+  text = rexp5_1.sub(' ',text) #apaloifh olwn twn tag tou keimenou me morfh p.x <a> h </a>
+  text = rexp5_2.sub(' ',text) #apaloifh olwn twn tag tou keimenou me morfh p.x <meta charset="utf-8" />
 
   text = rexp6.sub(myfunction,text) #antikatastash olwn twn akolouthown tou keimenou pou exoun morfh &amp; h &gt; h &lt; h &nbsp; vash tou pinaka pou exei dothei sthn ekfonhsh
 
-  text = rexp7.sub(' ',text) #antikatastash akolouthiwn sinexomenwn xarakthrwn whitespace me ena xarakthra whitespace
+  text = rexp7.sub(' ',text) #antikatastash kathe akolouthias sinexomenwn xarakthrwn whitespace tou keimenou me ena xarakthra whitespace
 
   print(text) #ektipwsh tou keimenou me oles tis tropopoihseis pou tou exoun ginei parapanw
 
